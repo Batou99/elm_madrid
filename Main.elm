d1a7ccf2a800a02963258498e113ccd2d5d2a4c2
Batou99 <lorenzo.lopez@intec.es>
2016-01-23 09:44:15 +0000

    Cabecera y pie

--- Main.elm
+++ Main.elm
@@ -1,6 +1,18 @@
 module Main where
 
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+pageHeader =
+  h1 [] [text "Temario"]
+
+
+pageFooter =
+  footer []
+    [a [href "https://github.com/Batou99/elm_madrid"]
+       [text "Generador de temarios"]
+    ]
+
 
 main = 
-  Html.text "Hola Mundo!"
+  div [id "container"] [pageHeader, pageFooter]
